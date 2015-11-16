1a0b289027317ef562ff1ec912d0b5cb41ed6e5f
Brachamul <barnaby.brachamul@gmail.com>
2015-11-16 18:32:14 +0100

    last update

--- freewall.js
+++ freewall.js
@@ -1,15 +1,5 @@
 $("head").append("<link rel='stylesheet' href='https://brachamul.github.io/sequoia-image-animator/freewall.css' type='text/css' media='screen'>");
 
-var wall = new freewall("#freewall");
-wall.fitWidth();
-wall.reset({
-    selector: '.brick',
-    animate: true,
-    onResize: function() {
-        wall.fitWidth();
-    }
-});
-
 // for mobile
 $('.brick').click(function(){
     $(this).toggleClass('active')
