1e0a40241a62458f22c068cc929f1fecb6c5c47f
Barnaby Brachamul <barnaby.brachamul@gmail.com>
2016-03-19 15:19:35 +0100

    tests

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+function loadStyle(href){
+    // avoid duplicates
+    for(var i = 0; i < document.styleSheets.length; i++){
+        if(document.styleSheets[i].href == href){
+            return;
+        }
+    }
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = href;
+    head.appendChild(link);
+}
+
+loadStyle('https://brachamul.github.io/sequoia-image-animator/styles.css')
+
 function runScript() {
 	// On regarde si jQuery est chargé, sinon on patiente un peu
 	if( window.jQuery ) {
@@ -8,8 +25,6 @@ function runScript() {
 				jQuery(this).toggleClass('active')
 			})
 
-			jQuery("head").append("<link rel='stylesheet' href='https://brachamul.github.io/sequoia-image-animator/styles.css' type='text/css' media='screen'>");
-
 		})
 
 	} else {
@@ -19,3 +34,27 @@ function runScript() {
 }
 
 runScript()
+
+
+
+//	function runScript() {
+//		// On regarde si jQuery est chargé, sinon on patiente un peu
+//		if( window.jQuery ) {
+//			// jQuery est chargé !
+//			jQuery( document ).ready(function(){
+//	
+//				jQuery('.bigimage-deploys').click(function(){
+//					jQuery(this).toggleClass('active')
+//				})
+//	
+//		//		jQuery("head").append("<link rel='stylesheet' href='' type='text/css' media='screen'>");
+//	
+//			})
+//	
+//		} else {
+//			// jQuery n'est pas chargé, on retente dans 50 milisecondes
+//			window.setTimeout( runScript, 50 )
+//		}
+//	}
+//	
+//	runScript()
